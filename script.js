54a67358b5546106920945b26af35cc905175829
nepomnasihnezont-crypto <nepomnasihnezont@gmail.com>
2026-09-05 20:00:16 +0700

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ function addNewItem() {
     const sizes = document.getElementById('itemSizes').value.trim();
     const qty = document.getElementById('itemQty').value.trim();
 
-    // Проверяем, чтобы поля не были пустыми
     if (!name || !sizes || !qty) {
         alert('Пожалуйста, заполните все поля!');
         return;
@@ -12,7 +11,6 @@ function addNewItem() {
     const table = document.getElementById('catalogList');
     const art = Math.floor(1000 + Math.random() * 9000);
 
-    // Создаем новую строку таблицы
     const row = document.createElement('tr');
     row.innerHTML = `
         <td>${art}</td>
@@ -21,10 +19,8 @@ function addNewItem() {
         <td>${qty} шт.</td>
     `;
     
-    // Добавляем строку в таблицу
     table.appendChild(row);
 
-    // Очищаем поля ввода
     document.getElementById('itemName').value = '';
     document.getElementById('itemSizes').value = '';
     document.getElementById('itemQty').value = '';
